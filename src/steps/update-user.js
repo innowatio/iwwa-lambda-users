19fdd7b5447957a0bff11d45ff30275d47f8e031
Manuel Serra <nitroin@hotmail.com>
2016-09-05 11:29:59 +0200

    Added logs

--- src/steps/update-user.js
+++ src/steps/update-user.js
@@ -1,5 +1,7 @@
 import {findUser, updateUser} from "../services/mongodb";
 
+import log from "../services/logger";
+
 export async function updateExistingUser(user) {
     const savedUser = await findUser(user.uid);
 
@@ -8,6 +10,10 @@ export async function updateExistingUser(user) {
         const updatedUser = {};
         user.sites ? updatedUser.sites = user.sites : null;
         user.roles ? updatedUser.roles = user.roles : null;
+
+        log.info({
+            updatedUser
+        }, `Updating user ${user.uid}`);
 
         await updateUser(user.uid, updatedUser);
     }
